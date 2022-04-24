ac9d8674cdf82e4511996e14ba0a5904a68c3961
Nick Nicolaou <nicolaou.nick@gmail.com@>
2022-04-24 22:03:18 +0300

    remove reference to unused packages

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,11 +1,7 @@
 package com.example.demo.controllers;
 
-import java.io.IOException;
-import java.util.Optional;
 
-import com.splunk.TcpInput;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
